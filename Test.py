8a62117e5b2ab1ce4d0ba878523d4ac46517b9fd
Young Developper <odjickfelix@gmail.com>
2021-04-07 20:32:29 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
+
+os.system("cls")
 
 PATH = r"C:\Users\petit\Documents\chromedriver" ##insert path here
 
@@ -13,3 +16,7 @@ driver.maximize_window()
 driver.get("https://" + destination)
 
 driver.maximize_window()
+
+os.system("cls")
+
+print("Opened: https://" + destination)
